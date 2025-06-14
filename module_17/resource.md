23da07f59bc70bdb0b920ca42ed8c84f4909ccc6
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-06-14 22:29:11 +0600

    17-3

---
+++ module_17/resource.md
@@ -0,0 +1,2 @@
+ https://github.com/Apollo-Level2-Web-Dev/advanced-note-app-with-mongoose/tree/module-6
+
---
+++ module_17/src/server.ts
@@ -0,0 +1,23 @@
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+
+let server: Server;
+
+const PORT = 5000;
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://todoapp:todoapp@cluster0.ty0stg3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("Connected to MongoDB Using Mongoose!!");
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
